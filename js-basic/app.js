dd56482389dcf88ef503877635abfc2a073f87c6
Evening <evening@Admins-iMac-4.local>
2022-11-28 17:31:49 +0800

    java

---
+++ js-basic/app.js
@@ -0,0 +1,35 @@
+console.log("hello world");
+
+
+
+
+
+// comments can make code readable
+// Javascript lesson begin
+/*Comments can make code 
+readable*/
+let nas = 30;
+let ner = "Namsrai";
+let as = true;
+let asas = undefined;
+let baba = null;
+console.log(typeof (baba), typeof(asas), typeof(nas), typeof(ner), typeof(as))
+let dsds = undefined;
+let qwe = 100;
+let firstname = "khur";
+let lastname = "khur";
+let maritalstatus = undefined;
+let country = undefined;
+let age = undefined;
+let first_name = null, last_name = undefined, marital_status = undefined, country_con = undefined, age_ag = undefined;
+let myage = 30;
+let yourage = 30;
+let a = 20;
+let b = 40;
+let c = 50;
+console.log("P=",a+b+c );
+let fff = 456;
+console.log()
+
+
+
