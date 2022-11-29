54a907a0c35e87d72cbece12945ceae9586c5f6a
Evening <evening@Admins-iMac-4.local>
2022-11-29 16:52:58 +0800

    condition

---
+++ js-basic/aa.js
@@ -0,0 +1,27 @@
+let weeknumber = 3;
+let weekname;
+ if  (weeknumber == 1) {
+    weekname = "Monday";
+ }
+ else if (weeknumber == 2) {
+    weekname = "thusday";
+ }
+ else if (weeknumber == 3) {
+    weekname = "Wednesday";
+ }
+ else if (weeknumber == 4) {
+    weekname = "Thursday";
+ }
+ else if (weeknumber == 5) {
+    weekname = "Friday";
+ }
+ else if (weeknumber == 6) {
+    weekname = "Saturday";
+ }
+ else if (weeknumber == 7) {
+    weekname = "Sunday";
+ }
+ else {
+    weekname = "Wrong number";
+ }
+ console.log(weekname);
