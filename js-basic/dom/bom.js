a59b394a163ced231f8119e63254c89d9d4d6aab
Evening <evening@Admins-iMac-4.local>
2022-12-12 17:29:42 +0800

    com

--- js-basic/dom/bom.js
+++ js-basic/dom/bom.js
@@ -1,6 +1,6 @@
 const colors = ["red", "blue", "green"];
 let colorIndex = 0;
-const body = document.getElementsByTagName('body')[0];
+const tan = document.querySelector(".tab");
 const change = () => {
     // if(colorIndex >=3){
     //     colorIndex = 0;
@@ -8,8 +8,9 @@ const change = () => {
     let red = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
-    body.style.backgroundColor = `rgb(${red},${blue},${grenn})`;
+    tan.style.backgroundColor = `rgb(${red},${blue},${green})`;
     // body.style = `background-color: ${colors[colorIndex]}`;
     // colorIndex++;
+    console.log(tab)
 };
 btm.addEventListener("click", change);
